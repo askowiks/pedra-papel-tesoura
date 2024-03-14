2a8de60b1b38465371bfca555ea72f69b61c1832
Welder Askowiks <72799464+askowiks@users.noreply.github.com>
2024-03-14 14:29:06 -0300

    Inserindo efeitos de som

--- main.ts
+++ main.ts
@@ -3,9 +3,12 @@ input.onGesture(Gesture.Shake, function () {
     x = randint(0, 2)
     if (x == 0) {
         basic.showIcon(IconNames.SmallSquare)
+        music.play(music.stringPlayable("C D E F G A B C5 ", 303), music.PlaybackMode.UntilDone)
     } else if (x == 1) {
         basic.showIcon(IconNames.Square)
+        music.play(music.stringPlayable("A F E F D G E F ", 307), music.PlaybackMode.UntilDone)
     } else {
         basic.showIcon(IconNames.Scissors)
+        music.play(music.stringPlayable("G B A G C5 B A B ", 319), music.PlaybackMode.UntilDone)
     }
 })
